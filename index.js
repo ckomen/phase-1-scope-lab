2449c12f6a4210f90ff8b299f068a4333418e595
caren komen <cjepkorir09@gmail.com>
2024-12-04 18:20:57 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+beforeEach(function () {
+  customerName = "bob";
+});
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  window.bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = "some initial value";
+
+function changeLeastFavoriteCustomer() {
+  try {
+    leastFavoriteCustomer = "new value";
+  } catch (error) {
+    throw new Error("Assignment to constant variable.");
+  }
+}
+
+console.log(customerName);
+upperCaseCustomerName();
+console.log(customerName);
+
+setBestCustomer();
+console.log(bestCustomer);
+
+overwriteBestCustomer();
+console.log(bestCustomer);
+
+console.log(leastFavoriteCustomer);
+changeLeastFavoriteCustomer();
